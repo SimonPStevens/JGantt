497c1defd6db4e10e0e32b6dda7f0a264ca42297
Simon P Stevens <simonpstevens@gmail.com>
2019-03-30 22:48:00 +0000

    Added better visuals for weekend days.

---
+++ JGantt/DateTimeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JGantt
+{
+    public static class DateTimeExtensions
+    {
+        public static bool IsWeekend(this DateTime date)
+        {
+            return date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday;
+        }
+    }
+}
